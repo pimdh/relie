3d0929f235aff8bceac67f3b4b1c5daf23d5999e
Pim de Haan <pimdehaan@gmail.com>
2018-10-01 14:10:33 -0700

    First working multimodal SO3 distribution!

--- relie/experiments/so3_multimodal_conditional_flow.py
+++ relie/experiments/so3_multimodal_conditional_flow.py
@@ -40,7 +40,7 @@ generation_group_distr = SO3Prior(dtype=torch.double, device=device)
 # Distribution to create noisy observations: p(G'|G)=n @ G, n \sim exp^*(N(0, .1))
 noise_alg_distr = Normal(
     torch.zeros(3).double().to(device),
-    torch.full((3, ), .1).double().to(device))
+    torch.full((3, ), .001).double().to(device))
 noise_group_distr = LDTD(noise_alg_distr, SO3ExpTransform())
 
 # Sample true and noisy group actions
@@ -55,30 +55,25 @@ rep_copies = 1
 x_dims = (max_rep_degree + 1)**2 * rep_copies
 # x_zero = torch.randn((max_rep_degree + 1)**2, rep_copies, device=device)
 x_zero = [
-    0.9749450087547302,
-    1.320214033126831,
-    2.7400097846984863,
-    0.6298772692680359,
-    0.8593451380729675,
-    0.6799159646034241,
-    -0.4878562390804291,
-    1.2094298601150513,
-    0.009278437122702599,
-    -1.52178156375885,
-    1.634827971458435,
-    -1.2686760425567627,
-    1.8586041927337646,
-    1.0522747039794922,
-    -0.7130511403083801,
-    0.3419789671897888]
+    0.9749450087547302, 1.320214033126831, 2.7400097846984863,
+    0.6298772692680359, 0.8593451380729675, 0.6799159646034241,
+    -0.4878562390804291, 1.2094298601150513, 0.009278437122702599,
+    -1.52178156375885, 1.634827971458435, -1.2686760425567627,
+    1.8586041927337646, 1.0522747039794922, -0.7130511403083801,
+    0.3419789671897888
+]
 x_zero = torch.tensor(x_zero, device=device)[:, None]
 
 # Make symmetrical
+symmetry_group_size = 3
 symmetry_group = so3_exp(
-    torch.tensor([[np.pi / 2 * i, 0, 0] for i in range(4)], device=device).double())
+    torch.tensor(
+        [[2 * np.pi / symmetry_group_size * i, 0, 0]
+         for i in range(symmetry_group_size)],
+        device=device).double())
 x_data = block_wigner_matrix_multiply(
-    so3_matrix_to_eazyz(symmetry_group).float(), x_zero.expand(4, -1, -1),
-    max_rep_degree)
+    so3_matrix_to_eazyz(symmetry_group).float(),
+    x_zero.expand(symmetry_group_size, -1, -1), max_rep_degree)
 x_zero = x_data.mean(0)
 
 # Act with group
@@ -99,7 +94,7 @@ class Flow(nn.Module):
         self.d_residue = 1
         self.d_transform = 2
         self.nets = nn.ModuleList([
-            MLP(self.d_residue + d_conditional, 2 * self.d_transform, 50, 2)
+            MLP(self.d_residue + d_conditional, 2 * self.d_transform, 50, 3)
             for _ in range(n_layers)
         ])
         self._set_params()
@@ -159,12 +154,12 @@ class FlowDistr(nn.Module):
         return -log_prob
 
 
-flow_model = Flow(6, x_dims, 4)
+flow_model = Flow(6, x_dims, 12)
 model = FlowDistr(flow_model).to(device)
 optimizer = torch.optim.Adam(model.parameters())
 
 losses = []
-num_its = 5000
+num_its = 20000
 for it in range(num_its):
     x_batch, g_batch, _ = next(loader_iter)
     x_batch = x_batch.view(-1, x_dims)
@@ -177,7 +172,6 @@ for it in range(num_its):
     if it % 1000 == 0:
         print(f"Loss: {np.mean(losses[-1000:]):.4f}.")
 
-
 if num_its > 0:
     path = os.path.join('models', f'so3-multimodal-{int(time())}.pkl')
     torch.save(model.state_dict(), path)
@@ -187,20 +181,26 @@ if num_its > 0:
 
 # model.load_state_dict(torch.load('models/so3-multimodal-1538424738.pkl'))
 num_noise_samples = 1000
-g_zero = generation_group_distr.sample((1,))[0]
+g_zero = generation_group_distr.sample((1, ))[0]
 g_subgroup = symmetry_group @ g_zero[None]
 # g_subgroup = g_zero[None]
 x = block_wigner_matrix_multiply(
     so3_matrix_to_eazyz(g_zero[None].float()), x_zero[None], max_rep_degree)
 
 inferred_distr = model.distr(x.view(-1).expand(num_noise_samples, -1))
-samples = inferred_distr.sample((num_noise_samples,)).view(num_noise_samples, 9)
+samples = inferred_distr.sample((num_noise_samples, )).view(
+    num_noise_samples, 9)
 # pca = PCA(3).fit(g_subgroup.view(4, 9))
 pca = PCA(3).fit(samples)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.scatter(*pca.transform(samples).T, label="Inferred", alpha=.2)
-ax.scatter(*pca.transform(g_subgroup.view(-1, 9)).T, label="Ground truth", s=100, alpha=1)
+ax.scatter(
+    *pca.transform(g_subgroup.view(-1, 9)).T,
+    label="Ground truth",
+    s=100,
+    alpha=1)
+ax.view_init(70, 30)
 plt.legend()
 plt.show()

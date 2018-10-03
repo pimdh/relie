80eb538da045855e0ffb3bb32088b9bd4215cb33
Pim de Haan <pimdehaan@gmail.com>
2018-10-02 21:11:21 -0700

    Experiment setup

--- relie/experiments/so3_multimodal_conditional_flow.py
+++ relie/experiments/so3_multimodal_conditional_flow.py
@@ -14,12 +14,14 @@ Data generation:
 - From uniform prior, we sample g \in G
 - We act: v_g = g(v)
 """
-import os
-from time import time
+import argparse
+import logging
 import numpy as np
+from relie.utils.experiment import setup_experiment
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.decomposition import PCA
+from sklearn.utils import Bunch
 
 import torch
 import torch.nn as nn
@@ -35,69 +37,15 @@ from relie.utils.modules import MLP, ConditionalModule, ToTransform
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-# Prior distribution p(G)
-generation_group_distr = SO3Prior(dtype=torch.double, device=device)
-
-# Distribution to create noisy observations: p(G'|G)=n @ G, n \sim exp^*(N(0, .1))
-noise_alg_distr = Normal(
-    torch.zeros(3).double().to(device),
-    torch.full((3, ), .1).double().to(device))
-noise_group_distr = LDTD(noise_alg_distr, SO3ExpTransform())
-
-# Sample true and noisy group actions
-num_samples = 100000
-noise_samples = noise_group_distr.sample((num_samples, ))
-group_data = generation_group_distr.sample((num_samples, ))
-group_data_noised = noise_samples @ group_data
-
-# Create original data
-max_rep_degree = 3
-rep_copies = 1
-x_dims = (max_rep_degree + 1)**2 * rep_copies
-# x_zero = torch.randn((max_rep_degree + 1)**2, rep_copies, device=device)
-x_zero = [
-    0.9749450087547302, 1.320214033126831, 2.7400097846984863,
-    0.6298772692680359, 0.8593451380729675, 0.6799159646034241,
-    -0.4878562390804291, 1.2094298601150513, 0.009278437122702599,
-    -1.52178156375885, 1.634827971458435, -1.2686760425567627,
-    1.8586041927337646, 1.0522747039794922, -0.7130511403083801,
-    0.3419789671897888
-]
-x_zero = torch.tensor(x_zero, device=device)[:, None]
-
-# Make symmetrical
-symmetry_group_size = 3
-symmetry_group = so3_exp(
-    torch.tensor(
-        [[2 * np.pi / symmetry_group_size * i, 0, 0]
-         for i in range(symmetry_group_size)],
-        device=device).double())
-x_data = block_wigner_matrix_multiply(
-    so3_matrix_to_eazyz(symmetry_group).float(),
-    x_zero.expand(symmetry_group_size, -1, -1), max_rep_degree)
-x_zero = x_data.mean(0)
-
-# Act with group
-angles = so3_matrix_to_eazyz(group_data_noised)
-x_data = block_wigner_matrix_multiply(angles.float(),
-                                      x_zero.expand(num_samples, -1, -1),
-                                      max_rep_degree)
-
-dataset = TensorDataset(x_data, group_data, group_data)
-loader = TensorLoader(dataset, 640, True)
-loader_iter = cycle(loader)
-
 
 class Flow(nn.Module):
-    def __init__(self, d, d_conditional, n_layers):
+    def __init__(self, d, d_conditional, n_layers, batch_norm=True, x_preprocess=False, net_layers=3):
         super().__init__()
         self.d = d
         self.d_residue = 1
         self.d_transform = d - self.d_residue
-        self.x_preprocess = False
-        self.batch_norm = True
 
-        if self.x_preprocess:
+        if x_preprocess:
             self.x_repr_dim = 9
             self.x_net = MLP(d_conditional, self.x_repr_dim, 50, 4, batch_norm=False)
         else:
@@ -108,14 +56,14 @@ class Flow(nn.Module):
             MLP(self.d_residue + self.x_repr_dim,
                 2 * self.d_transform,
                 50,
-                3,
+                net_layers,
                 batch_norm=False) for _ in range(n_layers)
         ])
         self._set_params()
         r = list(range(3))
         self.permutations = [r[i:] + r[:i] for i in range(3)]
 
-        if self.batch_norm:
+        if batch_norm:
             self.batch_norms = nn.ModuleList([
                 nn.BatchNorm1d(d) for _ in range(n_layers)])
         else:
@@ -145,26 +93,23 @@ class Flow(nn.Module):
             last_module.bias.data = torch.zeros_like(last_module.bias)
 
 
-algebra_support_radius = np.pi * 1.6
-
-intermediate_transform = ComposeTransform([
-    RadialTanhTransform(algebra_support_radius),
-    ToTransform(dict(dtype=torch.float32), dict(dtype=torch.float64))
-])
-
-
 class FlowDistr(nn.Module):
-    def __init__(self, flow):
+    def __init__(self, flow, algebra_support_radius=np.pi * 1.6):
         super().__init__()
         self.flow = flow
         self.register_buffer('prior_loc', torch.zeros(3))
         self.register_buffer('prior_scale', torch.ones(3))
+        self.intermediate_transform = ComposeTransform([
+            RadialTanhTransform(algebra_support_radius),
+            ToTransform(dict(dtype=torch.float32), dict(dtype=torch.float64))
+        ])
+        self.algebra_support_radius = algebra_support_radius
 
     def transforms(self, x):
         transforms = [
             self.flow(x).inv,
-            intermediate_transform,
-            SO3ExpCompactTransform(algebra_support_radius),
+            self.intermediate_transform,
+            SO3ExpCompactTransform(self.algebra_support_radius),
         ]
         return transforms
 
@@ -178,60 +123,132 @@ class FlowDistr(nn.Module):
         return -log_prob
 
 
-flow_model = Flow(3, x_dims, 12)
-model = FlowDistr(flow_model).to(device)
-# model.load_state_dict(torch.load('models/so3-multimodal-1538530160.pkl'))
-optimizer = torch.optim.Adam(model.parameters(), lr=1E-4)
+def gen_data(symmetry_group_size=3, noise=0.1):
+    # Prior distribution p(G)
+    generation_group_distr = SO3Prior(dtype=torch.double, device=device)
 
-losses = []
-num_its = 20000
-for it in range(num_its):
-    x_batch, g_batch, g_truth = next(loader_iter)
-    x_batch = x_batch.view(-1, x_dims)
-    loss = model.forward(x_batch, g_batch).mean()
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    losses.append(loss.item())
+    # Distribution to create noisy observations: p(G'|G)=n @ G, n \sim exp^*(N(0, .1))
+    noise_alg_distr = Normal(
+        torch.zeros(3).double().to(device),
+        torch.full((3, ), noise).double().to(device))
+    noise_group_distr = LDTD(noise_alg_distr, SO3ExpTransform())
 
-    for n, p in model.named_parameters():
-        assert torch.isnan(p).sum() == 0, f"NaN in parameters {n}"
+    # Sample true and noisy group actions
+    num_samples = 100000
+    noise_samples = noise_group_distr.sample((num_samples, ))
+    group_data = generation_group_distr.sample((num_samples, ))
+    group_data_noised = noise_samples @ group_data
 
-    if it % 1000 == 0:
-        print(f"It {it}. Loss: {np.mean(losses[-1000:]):.4f}.")
+    # Create original data
+    max_rep_degree = 3
+    rep_copies = 1
+    x_dims = (max_rep_degree + 1)**2 * rep_copies
+    # x_zero = torch.randn((max_rep_degree + 1)**2, rep_copies, device=device)
+    x_zero = [
+        0.9749450087547302, 1.320214033126831, 2.7400097846984863,
+        0.6298772692680359, 0.8593451380729675, 0.6799159646034241,
+        -0.4878562390804291, 1.2094298601150513, 0.009278437122702599,
+        -1.52178156375885, 1.634827971458435, -1.2686760425567627,
+        1.8586041927337646, 1.0522747039794922, -0.7130511403083801,
+        0.3419789671897888
+    ]
+    x_zero = torch.tensor(x_zero, device=device)[:, None]
 
-if num_its > 0:
-    path = os.path.join('models', f'so3-multimodal-{int(time())}.pkl')
-    torch.save(model.state_dict(), path)
-    print(f"Model saved to {path}")
+    # Make symmetrical
+    symmetry_group = so3_exp(
+        torch.tensor(
+            [[2 * np.pi / symmetry_group_size * i, 0, 0]
+             for i in range(symmetry_group_size)],
+            device=device).double())
+    x_data = block_wigner_matrix_multiply(
+        so3_matrix_to_eazyz(symmetry_group).float(),
+        x_zero.expand(symmetry_group_size, -1, -1), max_rep_degree)
+    x_zero = x_data.mean(0)
 
-# %%
-model.eval()
-# model.load_state_dict(torch.load('models/so3-multimodal-1538530160.pkl'))
-for _ in range(5):
-    num_noise_samples = 1000
-    g_zero = generation_group_distr.sample((1, ))[0]
-    g_subgroup = symmetry_group @ g_zero[None]
-    # g_subgroup = g_zero[None]
-    x = block_wigner_matrix_multiply(
-        so3_matrix_to_eazyz(g_zero[None].float()), x_zero[None], max_rep_degree)
+    # Act with group
+    angles = so3_matrix_to_eazyz(group_data_noised)
+    x_data = block_wigner_matrix_multiply(angles.float(),
+                                          x_zero.expand(num_samples, -1, -1),
+                                          max_rep_degree)
 
-    inferred_distr = model.distr(x.view(-1).expand(num_noise_samples, -1))
-    samples = inferred_distr.sample((num_noise_samples, )).view(
-        num_noise_samples, 9)
-    # pca = PCA(3).fit(g_subgroup.view(4, 9))
-    pca = PCA(3).fit(samples)
+    dataset = TensorDataset(x_data, group_data, group_data)
+    loader = TensorLoader(dataset, 640, True)
+    loader_iter = cycle(loader)
+    return Bunch(
+        loader_iter=loader_iter,
+        x_dims=x_dims,
+        generation_group_distr=generation_group_distr,
+        symmetry_group=symmetry_group,
+        x_zero=x_zero,
+        max_rep_degree=max_rep_degree,
+    )
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(*pca.transform(samples).T, label="Inferred", alpha=.2)
-    ax.scatter(
-        *pca.transform(g_subgroup.view(-1, 9)).T,
-        label="Ground truth",
-        s=100,
-        alpha=1,
-        zorder=2)
-    ax.view_init(70, 30)
-    plt.legend()
-    plt.show()
 
+def main():
+    parser = argparse.ArgumentParser('SO(3) multimodal conditional flow')
+    parser.add_argument('--name')
+    parser.add_argument('--flow_layers', type=int, default=18)
+    parser.add_argument('--noise', type=float, default=0.1)
+    args = parser.parse_args()
+
+    tb_writer, out_path = setup_experiment('flow', args.name, args)
+
+    data = gen_data(noise=args.noise)
+    flow_model = Flow(3, data.x_dims, args.flow_layers)
+    model = FlowDistr(flow_model).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=1E-4)
+
+    losses = []
+    num_its = 50000
+    for it in range(num_its):
+        x_batch, g_batch, g_truth = next(data.loader_iter)
+        x_batch = x_batch.view(-1, data.x_dims)
+        loss = model.forward(x_batch, g_batch).mean()
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        losses.append(loss.item())
+
+        for n, p in model.named_parameters():
+            assert torch.isnan(p).sum() == 0, f"NaN in parameters {n}"
+
+        if it % 1000 == 0:
+            logging.info(f"It {it}. Loss: {np.mean(losses[-1000:]):.4f}.")
+            tb_writer.add_scalar('loss', np.mean(losses[-1000:]), it)
+
+    if num_its > 0:
+        path = out_path(filename='model.pkl')
+        torch.save(model.state_dict(), path)
+        logging.info(f"Model saved to {path}")
+
+    model.eval()
+    for i in range(5):
+        num_noise_samples = 1000
+        g_zero = data.generation_group_distr.sample((1, ))[0]
+        g_subgroup = data.symmetry_group @ g_zero[None]
+        # g_subgroup = g_zero[None]
+        x = block_wigner_matrix_multiply(
+            so3_matrix_to_eazyz(g_zero[None].float()), data.x_zero[None], data.max_rep_degree)
+
+        inferred_distr = model.distr(x.view(-1).expand(num_noise_samples, -1))
+        samples = inferred_distr.sample((num_noise_samples, )).view(
+            num_noise_samples, 9)
+        # pca = PCA(3).fit(g_subgroup.view(4, 9))
+        pca = PCA(3).fit(samples)
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(*pca.transform(samples).T, label="Inferred", alpha=.2)
+        ax.scatter(
+            *pca.transform(g_subgroup.view(-1, 9)).T,
+            label="Ground truth",
+            s=100,
+            alpha=1)
+        ax.view_init(70, 30)
+        plt.legend()
+        plt.savefig(out_path(category='imgs', filename=f'{i}.png'))
+        plt.show()
+
+
+if __name__ == '__main__':
+    main()
---
+++ relie/utils/experiment.py
@@ -0,0 +1,71 @@
+import matplotlib
+import os
+import sys
+from os import environ
+import logging
+from pprint import pformat
+import json
+import secrets
+from datetime import datetime
+from tensorboardX import SummaryWriter
+from subprocess import check_output
+
+if not environ.get('DISPLAY', ''):
+    matplotlib.use("Agg")
+
+
+def setup_logging(path):
+    formatter = logging.Formatter("%(message)s")
+    root_logger = logging.getLogger()
+    root_logger.handlers = []
+
+    file_handler = logging.FileHandler(path)
+    file_handler.setFormatter(formatter)
+    root_logger.addHandler(file_handler)
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(formatter)
+    root_logger.addHandler(console_handler)
+
+    root_logger.setLevel(logging.INFO)
+
+
+def setup_experiment(experiment_name, run_name, args):
+    if 'RESULTS_PATH' in environ:
+        out_dir = environ['RESULTS_PATH']
+    else:
+        out_dir = './outputs'
+
+    t = str(int(datetime.utcnow().timestamp())) + secrets.token_hex(2)
+    identifier = f"{run_name}-{t}" if run_name else t
+
+    def out_path(category=None, filename=None):
+        path = os.path.join(out_dir, experiment_name, identifier)
+        if category is not None:
+            path = os.path.join(path, category)
+        os.makedirs(path, exist_ok=True)
+        if filename is not None:
+            return os.path.join(path, filename)
+        return path
+
+    setup_logging(out_path(filename='out.log'))
+
+    commit = check_output(['git', 'describe', '--always', '--dirty'])\
+        .decode('utf-8').strip()
+
+    if 'dirty' in commit and run_name is not None:
+        raise RuntimeError("Try to run in dirty commit with run name")
+
+    if not isinstance(args, dict):
+        args = vars(args)
+    data = {
+        **args, 'identifier': identifier,
+        'source_commit': commit
+    }
+    logging.info(pformat(data))
+    with open(out_path(filename='config.json'), 'w') as f:
+        json.dump(data, f, indent=2)
+
+    tb_writer = SummaryWriter(out_path(category='tb'))
+
+    return tb_writer, out_path

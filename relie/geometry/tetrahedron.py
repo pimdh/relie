e61ea70e0f0c30c55e66354244a62d766f7bf898
Pim de Haan <pimdehaan@gmail.com>
2018-10-04 02:44:50 -0700

    Removed non-orientation perserving symmetries from tetrahedron

--- relie/geometry/tetrahedron.py
+++ relie/geometry/tetrahedron.py
@@ -4,6 +4,7 @@ Equals Permutation group of order 4.
 """
 import numpy as np
 from itertools import permutations as permutation_fn
+from .utils import permutation_matrices
 
 
 def tetrahedron_coordinates():
@@ -16,4 +17,11 @@ def tetrahedron_coordinates():
 
 
 def tetrahedron_permutations():
-    return permutation_fn(range(4))
+    """
+    All permutations with positive determinant permutation matrix.
+    :return: Array of shape (4!/2, 4)
+    """
+    permutations = list(permutation_fn(range(4)))
+    matrices = permutation_matrices(permutations)
+    return [p for p, m in zip(permutations, matrices) if np.linalg.det(m) > 0]
+

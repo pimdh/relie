33ce8375ea37972371ab24ac81013b98838e0a55
Jared Lafer <jared.lafer@gmail.com>
2022-05-23 14:11:27 -0500

    setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='relie',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'torch',
+        'pytorch3d',
+        'numpy'
+        'Pillow',
+        'tensorboardX',
+        'scikit-learn',
+        'matplotlib'
+    ]
+)

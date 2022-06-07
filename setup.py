491b0d4f8cc59304139a8bbe7a268b7a965a2990
Jared Lafer <jared.lafer@gmail.com>
2022-06-07 12:51:51 -0500

    update setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='relie',
-    version='1.1',
+    version='1.2',
     packages=find_packages(),
     install_requires=[
         'torch',
